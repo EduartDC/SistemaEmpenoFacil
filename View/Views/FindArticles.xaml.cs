b5b976706e8738130a82fc630f79e4daaf60f95e
Jona <zS19023589@estudiantes.uv.mx>
2023-04-19 14:52:57 -0600

    se agrego articlesDAO

--- View/Views/FindArticles.xaml.cs
+++ View/Views/FindArticles.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * Autor: Jonathan Hernandez Martinez
+ */
+using DataAcces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +16,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BusinessLogic;
+using View.Properties;
 
 namespace View.Views
 {
-    /// <summary>
-    /// Lógica de interacción para FindArticles.xaml
-    /// </summary>
+
     public partial class FindArticles : Page
     {
+        private List<Belongings_Articles> articles = new List<Belongings_Articles>();
         public FindArticles()
         {
             InitializeComponent();
+            LoadArticles();
         }
 
-       
+        private void LoadArticles()
+        {
+            int code = 0;
+            articles.Clear();
+            (code, articles) = ArticleDAO.getArticles();
+            MessageBox.Show(code.ToString());
+            if (code == MessageCode.SUCCESS)
+            {
+                LoadTable();
+            }
+            else
+            if (code == MessageCode.CONNECTION_ERROR)
+            {
+                ErrorManager.ShowError(MessageError.CONNECTION_ERROR);
+                //codigo para cerrar page
+            }
+        }
+
+        private void LoadTable()
+        {
+            dgArticles.Items.Clear();
+            dgArticles.ItemsSource = articles;
+        }
     }
 }
