e343c174c3536d51daa111ac6eb9ea8f77899d48
Axelpm <axelpm28@gmail.com>
2023-04-18 18:04:13 -0600

    Se agregaron los casos de uso modificar metricas y consultar lista negra

--- BusinessLogic/CustomerDAO.cs
+++ BusinessLogic/CustomerDAO.cs
@@ -1,6 +1,7 @@
 ﻿using DataAcces;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace BusinessLogic
 {
     public class CustomerDAO
     {
-
+        private static NewLog _log = new NewLog();
         public static int AddCustomer(Customer newCustomer)
         {
             var result = MessageCode.ERROR;
@@ -37,6 +38,44 @@ namespace BusinessLogic
 
             }
             return result;
+        }
+
+        public static List<Domain.Customer> RecoverCustomers()
+        {
+            List<Domain.Customer> resultCustomers = new List<Domain.Customer>();
+            try
+            {
+                using (var database = new ConnectionModel())
+                {
+                    var customer = (from Customers in database.Customers
+                                    where
+                                    Customers.blackList == true
+                                    select Customers).ToList();
+
+                    foreach (DataAcces.Customer customer1 in customer)
+                    {
+                        Domain.Customer newCustomer = new Domain.Customer();
+                        newCustomer.idCustomer = customer1.idCustomer;
+                        newCustomer.curp = customer1.curp;
+                        newCustomer.blackList = customer1.blackList;
+                        newCustomer.address = customer1.address;
+                        newCustomer.firstName = customer1.firstName;
+                        newCustomer.lastName = customer1.lastName;
+                        newCustomer.identification = customer1.identification;
+                        newCustomer.telephonNumber = customer1.telephonNumber;
+                        resultCustomers.Add(newCustomer);
+                    }
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                _log.Add(ex.ToString());
+            }
+            catch (EntityException ex)
+            {
+                _log.Add(ex.ToString());
+            }
+            return resultCustomers;
         }
     }
 }
