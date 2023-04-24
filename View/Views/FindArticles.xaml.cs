8f52dbe121cef9e358108fb4dfff61c62d77726b
Jona <zS19023589@estudiantes.uv.mx>
2023-04-24 00:36:03 -0600

    1)se agregaron los llenados de campos en CreateCotract(a expexcion de dos). 2)se agregaron dos mensajes de error en MessageError. 3) se agrego la page GiveProfitToCustomer 4)se agreggo un menuItem adicional al MenuView para acceder a la GiveProfitToCustomer

--- View/Views/FindArticles.xaml.cs
+++ View/Views/FindArticles.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BusinessLogic;
 using View.Properties;
+using System.Windows.Controls.Primitives;
 
 namespace View.Views
 {
@@ -25,10 +26,13 @@ namespace View.Views
     public partial class FindArticles : Page
     {
         private List<Belongings_Articles> articles = new List<Belongings_Articles>();
+        private Boolean datePickerEnabled = true;
+
         public FindArticles()
         {
             InitializeComponent();
             LoadArticles();
+            LoadCategories();
         }
 
         private void LoadArticles()
@@ -48,11 +52,64 @@ namespace View.Views
                 //codigo para cerrar page
             }
         }
+        private void LoadCategories()
+        {
+            List<string> categoriesList = new List<string> { "Selecciona una categoria", "Joyeria", "Relojeria", "Herramientas" };
+            cbCategory.ItemsSource = categoriesList;
+            cbCategory.SelectedIndex = 0;
+        }
 
         private void LoadTable()
         {
             dgArticles.Items.Clear();
             dgArticles.ItemsSource = articles;
+            dgArticles.RowHeight = double.NaN;//tamaño de filas automaticas al contenido
+        }
+
+        private void Btn_EditArticle(object sender, RoutedEventArgs e)
+        {
+            if (SelectedItem())
+            {
+                // Container.NavigationService.Navigate(new EditArticle(articles[dgArticles.SelectedIndex]))
+            }
+            else
+                ErrorManager.ShowError(MessageError.ITEM_NOT_SELECTED);
+        }
+
+        private bool SelectedItem()
+        {
+            if (dgArticles.SelectedIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        private void btn_CleanFilters(object sender, RoutedEventArgs e)
+        {
+            LoadArticles();
+        }
+
+        private void btn_FilterArticles(object sender, RoutedEventArgs e)
+        {
+            string filter = tbSearchField.Text;
+            var result = dgArticles.Items.Cast<Belongings_Articles>().Where(item => item.barCode.Contains(filter));
+            result = dgArticles.Items.Cast<Belongings_Articles>().Where(item => item.Belonging.serialNumber.Contains(filter));
+           // if(cbCategory.SelectedIndex)
+            //miDataGrid.ItemsSource = datosFiltrados;
+        }
+
+        private void Cb_CalendarEnabled(object sender, RoutedEventArgs e)
+        {
+
+            dpDate.IsEnabled = false;
+            datePickerEnabled = false;
+
+        }
+
+        private void bt_dateUnabled(object sender, RoutedEventArgs e)
+        {
+            dpDate.IsEnabled = true;
+            datePickerEnabled = true;
         }
     }
 }
