90e0e8456603c63add24cf38e994c9ffdecfdf97
Axelpm <axelpm28@gmail.com>
2023-04-25 22:06:13 -0600

    Cambios para solucionar error en imagenes

--- View/Views/CreateCustomerRecord.xaml.cs
+++ View/Views/CreateCustomerRecord.xaml.cs
@@ -20,10 +20,67 @@ namespace View.Views
     /// </summary>
     public partial class CreateCustomerRecord : Page
     {
+
+        private bool image1 = false;
+        private bool image2 = false;
+        BitmapImage bitMap3 = new BitmapImage();
+        BitmapImage bitMap4 = new BitmapImage();
+
         public CreateCustomerRecord()
         {
             InitializeComponent();
+            comBox_Identification.Items.Add("INE");
+            comBox_Identification.Items.Add("CURP");
+            comBox_Identification.Items.Add("Cartilla militar");
         }
 
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_AddImage1_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "Archivos de imagen (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+            bool? result = ofd.ShowDialog();
+            if (result == true)
+            {
+                string ruta = ofd.FileName;
+                Image imgOne = new Image();
+                BitmapImage bitMap = new BitmapImage();
+                bitMap.BeginInit();
+                bitMap.UriSource = new Uri(ruta);
+                bitMap.EndInit();
+                componentImageOne.Source = bitMap;
+                image1 = false;
+            }
+
+            }
+
+        private void Btn_AddImage2_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "Archivos de imagen (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+            bool? result = ofd.ShowDialog();
+            if (result == true)
+            {
+                string ruta = ofd.FileName;
+                Image imgOne = new Image();
+                BitmapImage bitMap = new BitmapImage();
+                bitMap.BeginInit();
+                bitMap.UriSource = new Uri(ruta);
+                bitMap.EndInit();
+                componentImageTwo.Source = bitMap;
+                image2 = false;
+            }
+        }
+
+        
     }
 }
