bd6388101111ba40b0c4730324c92df4fd42223a
RafaelMC23 <zs19023586@estudiantes.uv.mx>
2023-04-27 18:17:56 -0600

    staffDAO y Login window

---
+++ BusinessLogic/StaffDAO.cs
@@ -0,0 +1,36 @@
+﻿using DataAcces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public static class StaffDAO
+    {
+        public static (int, string) validateUser(string userName, string password)
+        {
+            if (Utilitys.VerifyConnection())
+            {
+                using (var connection = new ConnectionModel())
+                {
+                    List<Staff> list = connection.Staffs.ToList();
+
+                    foreach (var staff in list)
+                    {
+                        if (staff.userName.Equals(userName) && staff.password.Equals(password))
+                        {
+                            return (MessageCode.SUCCESS, staff.rol);
+                        }
+
+
+                    }
+                    return (MessageCode.ERROR_USER_NOT_FOUND, MessageError.USER_AND_PASSWORD_NOT_FOUND);
+                }
+            }
+            else
+                return (MessageCode.CONNECTION_ERROR, "");
+        }
+    }
+}
