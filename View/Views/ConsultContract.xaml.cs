f01b8a2aee58d2cbce2b862642935131073ca355
Axelpm <axelpm28@gmail.com>
2023-05-24 20:28:31 -0600

    Se agrego ConsultContract

---
+++ View/Views/ConsultContract.xaml.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using View.Properties;
+
+namespace View.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ConsultContract.xaml
+    /// </summary>
+    public partial class ConsultContract : Window
+    {
+        public ConsultContract()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Salir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async Task SetInformationAsync(int idContract)
+        {
+            try
+            {
+                var contract = await Task.Run(() => ContractDAO.GetContractsDomainAsync(idContract));
+                if (contract == null)
+                {
+                    ErrorManager.ShowWarning("No se encontro el contrato.");
+                }
+                else
+                {
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorManager.ShowError(ex.Message);
+            }
+        }
+    }
+}
